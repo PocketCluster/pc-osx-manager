a609f0730c29b0ce33e15f170609fde586e5509d
stkim1 <stkim1@colorfulglue.com>
2015-10-21 20:13:27 +0900

    update

---
+++ manager/manager/Common/Serial/DeviceSerialNumber.h
@@ -0,0 +1,12 @@
+//
+//  DeviceSerialNumber.h
+//  manager
+//
+//  Created by Almighty Kim on 10/21/15.
+//  Copyright © 2015 io.pocketcluster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern CFTypeRef CopySerialNumber(void);
+
