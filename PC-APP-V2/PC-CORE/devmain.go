cf94c6e9096774e4f06a643451d72204538b0145
stkim1 <stkim1@colorfulglue.com>
2016-09-17 21:33:45 +0900

    daily commit

--- PC-APP-V2/PC-CORE/devmain.go
+++ PC-APP-V2/PC-CORE/devmain.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+    "crypto/tls"
+    "github.com/docker/swarm/api"
+)
 func main() {
+    var tlsConfig *tls.Config;
 
+    server := api.NewServer([]string{"0.0.0.0:2375"},tlsConfig)
+    server.ListenAndServe()
 }
