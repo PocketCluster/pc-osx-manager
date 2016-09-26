fa6b2ff1b9cefe886353294caaef97a60d356882
stkim1 <stkim1@colorfulglue.com>
2016-09-26 19:59:47 +0900

    pc-node multicast over wifi

---
+++ PC-APP-V2/pc-node-agent/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "log"
+    "fmt"
+    "time"
+
+    "github.com/stkim1/pc-node-agent/mcast"
+)
+
+func main() {
+    client, err := mcast.NewClient(); if err != nil {
+        log.Fatal("[ERR] cannot initate Multi-cast client")
+    }
+
+    recvMsg := make(chan []byte, 32)
+    param := mcast.DefaultParams([]byte("this is message"), recvMsg)
+    client.Query(param)
+
+    go func() {
+        for entry := range recvMsg {
+            fmt.Printf("Got new entry: %s\n", string(entry))
+        }
+    }()
+
+    // Start the lookup
+    client.Query(param)
+
+    for {
+        time.Sleep(time.Second)
+    }
+}
