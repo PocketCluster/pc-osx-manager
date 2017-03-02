1b14c6a9f29db9551a35a633cb0f06ff9915ed20
stkim1 <stkim1@colorfulglue.com>
2017-03-02 09:44:42 +0900

    refactored

--- PC-APP-V2/PC-CORE/exec/compmain/main.go
+++ PC-APP-V2/PC-CORE/exec/compmain/main.go
@@ -1,21 +1,24 @@
-package compose
+package main
 
 import (
-    "log"
-
-    "golang.org/x/net/context"
+    "os"
 
     "github.com/docker/libcompose/docker"
     "github.com/docker/libcompose/docker/ctx"
     "github.com/docker/libcompose/project"
     "github.com/docker/libcompose/project/options"
+
+    "golang.org/x/net/context"
+    log "github.com/Sirupsen/logrus"
+    "github.com/davecgh/go-spew/spew"
 )
 
-func LaunchService() {
+func main() {
+    log.SetOutput(os.Stdout)
     project, err := docker.NewProject(&ctx.Context{
         Context: project.Context{
-            ComposeFiles: []string{"docker-compose.yml"},
-            ProjectName:  "my-compose",
+            ComposeFiles: []string{"pocket-deploy-original.yml"},
+            ProjectName:  "pocket-hadoop",
         },
     }, nil)
 
@@ -23,8 +26,10 @@ func LaunchService() {
         log.Fatal(err)
     }
 
-    err = project.Up(context.Background(), options.Up{})
+    log.Info(spew.Sdump(project))
+    return
 
+    err = project.Up(context.Background(), options.Up{})
     if err != nil {
         log.Fatal(err)
     }
