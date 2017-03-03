53c04d13e7760510bbdcd8fb6d8372c77cabcee2
stkim1 <stkim1@colorfulglue.com>
2017-03-04 04:06:17 +0900

    comp main

--- PC-APP-V2/PC-CORE/exec/compmain/main.go
+++ PC-APP-V2/PC-CORE/exec/compmain/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
     "os"
+    "io/ioutil"
 
     "github.com/docker/libcompose/docker"
     "github.com/docker/libcompose/docker/ctx"
+    "github.com/docker/libcompose/docker/client"
     "github.com/docker/libcompose/project"
     "github.com/docker/libcompose/project/options"
 
@@ -15,13 +17,40 @@ import (
 
 func main() {
     log.SetOutput(os.Stdout)
-    project, err := docker.NewPocketProject(&ctx.Context{
-        Context: project.Context{
-            ComposeFiles: []string{"pocket-deploy.yml"},
-            ProjectName:  "pocket-hadoop",
-        },
-    }, nil)
 
+    composeFile := "pocket-deploy.json"
+
+    composeBytes, err := ioutil.ReadFile("pocket-deploy.json")
+    if err != nil && !os.IsNotExist(err) {
+        log.Fatal("Failed to open the compose file: %s", composeFile)
+    }
+    caCert, err  := ioutil.ReadFile("/Users/almightykim/Workspace/DKIMG/CERT/ca-cert.pub")
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    tlsCert, err := ioutil.ReadFile("/Users/almightykim/Workspace/DKIMG/PC-MASTER/pc-master.cert")
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    tlsKey, err  := ioutil.ReadFile("/Users/almightykim/Workspace/DKIMG/PC-MASTER/pc-master.key")
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+
+    opts, err := client.NewPocketCientOption(caCert, tlsCert, tlsKey, "tcp://192.168.1.150:3376")
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    project, err := docker.NewPocketProject(&docker.PocketContext{
+        Context: &ctx.Context{
+            Context: project.Context{
+                ComposeFiles: []string{composeFile},
+                ProjectName:  "pocket-hadoop",
+            },
+        },
+        ClientOptions: opts,
+        Manifest: composeBytes,
+    }, nil)
     if err != nil {
         log.Fatal(err)
     }
