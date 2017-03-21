9cbacd1167f1c7bd4d5e5b1a04c5ff0f030a6c1d
stkim1 <stkim1@colorfulglue.com>
2017-03-21 13:15:08 +0900

    network noti added

--- PC-APP-V2/PC-APP/manager/manager/Application/AppDelegate.m
+++ PC-APP-V2/PC-APP/manager/manager/Application/AppDelegate.m
@@ -15,31 +15,55 @@
 #import "Util.h"
 #import "AppDelegate.h"
 #import "NativeMenu.h"
+#import "PCInterfaceStatus.h"
+#import "_cgo_export.h"
 
-@interface AppDelegate ()<SUUpdaterDelegate, NSUserNotificationCenterDelegate>
+bool
+pc_interface_list(PCNetworkInterface** interfaces, unsigned int count) {
+    NetworkChangeNotificationInterface(interfaces, count);
+    return true;
+}
+
+bool
+gateway_list(SCNIGateway** gateways, unsigned int count) {
+    NetworkChangeNotificationGateway(gateways, count);
+    return true;
+}
+
+@interface AppDelegate ()<SUUpdaterDelegate, NSUserNotificationCenterDelegate, PCInterfaceStatusNotification>
 @property (nonatomic, strong, readwrite) NativeMenu *nativeMenu;
 @property (nonatomic, strong) NSMutableArray *openWindows;
+@property (strong) PCInterfaceStatus *interfaceStatus;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    
-    // opened window list
-    self.openWindows = [[NSMutableArray alloc] init];
-    
     //initialize updates
     [[SUUpdater sharedUpdater] setDelegate:self];
     [[SUUpdater sharedUpdater] setSendsSystemProfile:[Util shouldSendProfileData]];
     [[SUUpdater sharedUpdater] checkForUpdateInformation];
     
+    self.interfaceStatus = [[PCInterfaceStatus alloc] initWithStatusAudience:self];
+    [self.interfaceStatus startMonitoring];
+    
+    interface_status_with_callback(&pc_interface_list);
+    gateway_status_with_callback(&gateway_list);
+    NSLog(@"\n--- --- --- CALLBACK C CALL ENDED --- --- ---");
+
+    
+    // opened window list
+    self.openWindows = [[NSMutableArray alloc] init];
+    
     //create popup and status menu item
     self.nativeMenu = [[NativeMenu alloc] init];
-    
+
     Log(@"Application Started");
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
+    [self.interfaceStatus stopMonitoring];
+    self.interfaceStatus = nil;
 }
 
 - (void)application:(NSApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
@@ -51,4 +75,14 @@
 
 - (void)removeOpenWindow:(id)window {
 }
+
+#pragma mark - PCInterfaceStatusNotification
+-(void)PCInterfaceStatusChanged:(PCInterfaceStatus *)monitor interfaceStatus:(PCNetworkInterface**)status count:(unsigned int)count {
+    pc_interface_list(status, count);
+}
+
+-(void)PCGatewayStatusChanged:(PCInterfaceStatus *)monitor gatewayStatus:(SCNIGateway**)status count:(unsigned int)count {
+    gateway_list(status, count);
+}
+
 @end
