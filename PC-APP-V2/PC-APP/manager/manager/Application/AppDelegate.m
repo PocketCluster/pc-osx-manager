945b7de38b88ab3d97c4c2a61827d83dc421116c
stkim1 <stkim1@colorfulglue.com>
2017-04-10 17:19:46 +0900

    net monitor refactor

--- PC-APP-V2/PC-APP/manager/manager/Application/AppDelegate.m
+++ PC-APP-V2/PC-APP/manager/manager/Application/AppDelegate.m
@@ -16,79 +16,15 @@
 
 #import "Util.h"
 #import "NativeMenu.h"
-#import "PCInterfaceStatus.h"
 #include "pc-core.h"
 
 #import "AppDelegate.h"
+#import "AppDelegate+Netmonitor.h"
 #import "AppDelegate+EventHandle.h"
 #import "AppDelegate+Notification.h"
 #import "AppDelegate+Sparkle.h"
 
-bool
-pc_interface_list(PCNetworkInterface** interfaces, unsigned int count) {
-#ifdef COMPARE_NATIVE_GO_OUTPUT
-    printf("\n\n---- total intefaces count %d ----\n\n", count);
-    for (unsigned int i = 0; i < count; i++) {
-        
-        PCNetworkInterface *iface = *(interfaces + i);
-        printf("wifiPowerOff : %d\n",iface->wifiPowerOff);
-        printf("isActive : %d\n",iface->isActive);
-        printf("isPrimary : %d\n",iface->isPrimary);
-        printf("addrCount: %d\n",iface->addrCount);
-        
-        if (iface->addrCount != 0) {
-            for (unsigned int i = 0; i < iface->addrCount; i++) {
-                SCNIAddress *addr = *(iface->address + i);
-                printf("\tflags  : %x\n", addr->flags);
-                printf("\tfamily : %d\n", addr->family);
-                printf("\tis_primary : %d\n", addr->is_primary);
-                printf("\taddr : %s\n", addr->addr);
-                printf("\tnetmask : %s\n", addr->netmask);
-                printf("\tbroadcast : %s\n", addr->broadcast);
-                printf("\tpeer : %s\n\t--------------------\n", addr->peer);
-            }
-        }
-        
-        printf("bsdName : %s\n",iface->bsdName);
-        printf("displayName: %s\n",iface->displayName);
-        printf("macAddress: %s\n",iface->macAddress);
-        printf("mediaType: %s\n--------------------\n",iface->mediaType);
-    }
-    if ([NSThread isMainThread]) {
-        printf("!!! THIS IS M.A.I.N THREAD!!!\n\n");
-    } else {
-        printf("!!! this not main thread!!!\n\n");
-    }
-#endif
-
-    NetworkChangeNotificationInterface(interfaces, count);
-    return true;
-}
-
-bool
-gateway_list(SCNIGateway** gateways, unsigned int count) {
-#ifdef COMPARE_NATIVE_GO_OUTPUT
-    printf("\n\n---- Total gateway count %d ----\n", count);
-    for (unsigned int i = 0; i < count; i++) {
-        SCNIGateway *gw = *(gateways + i);
-        printf("family : %d\n",gw->family);
-        printf("is_default : %d\n",gw->is_default);
-        printf("ifname : %s\n",gw->ifname);
-        printf("addr: %s\n",gw->addr);
-    }
-    if ([NSThread isMainThread]) {
-        printf("!!! THIS IS M.A.I.N THREAD!!!\n\n");
-    } else {
-        printf("!!! this not main thread!!!\n\n");
-    }
-#endif
-
-    NetworkChangeNotificationGateway(gateways, count);
-    return true;
-}
-
-
-@interface AppDelegate ()<NSUserNotificationCenterDelegate, PCInterfaceStatusNotification>
+@interface AppDelegate ()<NSUserNotificationCenterDelegate>
 @property (nonatomic, strong, readwrite) NativeMenu *nativeMenu;
 @property (nonatomic, strong) NSMutableArray *openWindows;
 @property (strong) PCInterfaceStatus *interfaceStatus;
@@ -141,8 +77,8 @@ gateway_list(SCNIGateway** gateways, unsigned int count) {
     // 10. refresh network status -> this might update OSX side as well, so we need UI to be working beforehand.
     // Plus, it delayed execution give a room to golang to be initialized
     Log(@"\n[NET] REFRESHING INTERFACE...\n");
-    interface_status_with_callback(&pc_interface_list);
-    gateway_status_with_callback(&gateway_list);
+    interface_status_with_callback(&PCUpdateInterfaceList);
+    gateway_status_with_callback(&PCUpdateGatewayList);
     // now let interface to be updated
     [self.interfaceStatus startMonitoring];
 
@@ -185,15 +121,6 @@ gateway_list(SCNIGateway** gateways, unsigned int count) {
 }
 
 - (void)removeOpenWindow:(id)window {
-}
-
-#pragma mark - PCInterfaceStatusNotification
--(void)PCInterfaceStatusChanged:(PCInterfaceStatus *)monitor interfaceStatus:(PCNetworkInterface**)status count:(unsigned int)count {
-    pc_interface_list(status, count);
-}
-
--(void)PCGatewayStatusChanged:(PCInterfaceStatus *)monitor gatewayStatus:(SCNIGateway**)status count:(unsigned int)count {
-    gateway_list(status, count);
 }
 
 @end
