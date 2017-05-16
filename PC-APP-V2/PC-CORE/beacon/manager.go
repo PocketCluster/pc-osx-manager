3ed711058ff70e7440cebd293344707cd8d8ee3a
stkim1 <stkim1@colorfulglue.com>
2017-05-17 05:24:19 +0900

    manager list check

--- PC-APP-V2/PC-CORE/beacon/manager.go
+++ PC-APP-V2/PC-CORE/beacon/manager.go
@@ -20,6 +20,7 @@ func NewBeaconManagerWithFunc(comm CommChannelFunc) BeaconManger {
 }
 
 func NewBeaconManager(comm CommChannel) BeaconManger {
+    // TODO find existing slave models and attach them
     return &beaconManger {
         commChannel:    comm,
         beaconList:     []MasterBeacon{},
@@ -33,9 +34,10 @@ type BeaconManger interface {
     Shutdown() error
 }
 
+// We might not need a locking mechanism as "select" statement will choose only "one input" at a time.
 type beaconManger struct {
-    commChannel    CommChannel
-    beaconList     []MasterBeacon
+    commChannel  CommChannel
+    beaconList   []MasterBeacon
 }
 
 func (b *beaconManger) TransitionWithBeaconData(beaconD ucast.BeaconPack) error {
@@ -51,13 +53,40 @@ func (b *beaconManger) TransitionWithBeaconData(beaconD ucast.BeaconPack) error 
 }
 
 func (b *beaconManger) TransitionWithSearchData(searchD mcast.CastPack) error {
-    // suppose we've sort out what this is.
+    var (
+        bcFound bool = false
+        mc MasterBeacon = nil
+        ts time.Time = time.Now()
+    )
+
     usm, err := slagent.UnpackedSlaveMeta(searchD.Message)
     if err != nil {
         return errors.WithStack(err)
     }
 
     log.Debugf("[SEARCH] FROM %v\n%v ", searchD.Address.IP.String(), spew.Sdump(usm))
+
+    for _, bc := range b.beaconList {
+        if bc.SlaveNode().MacAddress == usm.SlaveID {
+
+            // this beacon's state should be identified
+            err = bc.TransitionWithSlaveMeta(nil, usm, ts)
+
+            bcFound = true
+            break
+        }
+    }
+    if !bcFound {
+        mc, err = NewMasterBeacon(MasterInit, nil, b.commChannel)
+        if err != nil {
+            return errors.WithStack(err)
+        }
+        b.beaconList = append(b.beaconList, mc)
+        err = mc.TransitionWithSlaveMeta(nil, usm, ts)
+        if err != nil {
+            return errors.WithStack(err)
+        }
+    }
     return nil
 }
 
@@ -67,6 +96,6 @@ func (b *beaconManger) TransitionWithTimestamp(ts time.Time) error {
 }
 
 func (b *beaconManger) Shutdown() error {
-
+    b.commChannel = nil
     return nil
 }
