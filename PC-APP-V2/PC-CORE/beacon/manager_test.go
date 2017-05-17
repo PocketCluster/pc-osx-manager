eba21b39f82c33da7cf3334a3f0402d7460d6289
stkim1 <stkim1@colorfulglue.com>
2017-05-18 05:53:52 +0900

    note test added

--- PC-APP-V2/PC-CORE/beacon/manager_test.go
+++ PC-APP-V2/PC-CORE/beacon/manager_test.go
@@ -1,1 +1,36 @@
 package beacon
+
+import (
+    "testing"
+
+    log "github.com/Sirupsen/logrus"
+    . "gopkg.in/check.v1"
+    "github.com/stkim1/pc-core/model"
+)
+
+func TestRecord(t *testing.T) { TestingT(t) }
+
+type ManagerSuite struct {
+    dataDir     string
+}
+
+var _ = Suite(&ManagerSuite{})
+
+func (s *ManagerSuite) SetUpSuite(c *C) {
+    log.SetLevel(log.DebugLevel)
+}
+
+func (s *ManagerSuite) TearDownSuite(c *C) {
+}
+
+func (s *ManagerSuite) SetUpTest(c *C) {
+    var err error
+
+    s.dataDir = c.MkDir()
+    _, err = model.DebugRecordGatePrepare(s.dataDir)
+    c.Assert(err, IsNil)
+}
+
+func (s *ManagerSuite) TearDownTest(c *C) {
+    c.Assert(model.DebugRecordGateDestroy(s.dataDir), IsNil)
+}
--- PC-APP-V2/PC-CORE/model/slavenode_test.go
+++ PC-APP-V2/PC-CORE/model/slavenode_test.go
@@ -4,6 +4,7 @@ import (
     "fmt"
 
     . "gopkg.in/check.v1"
+    "github.com/pborman/uuid"
 )
 
 const (
@@ -24,25 +25,26 @@ func (s *RecordSuite) TestAllNodeCount(c *C) {
     c.Assert(len(nodes), Equals, allNodeCount)
 }
 
-/*
+
 func (s *RecordSuite) TestFindSingleNode(c *C) {
+    var (
+        uuidList []string = []string{}
+    )
     for i := 0; i < allNodeCount; i++ {
+        ui := uuid.New()
         sl := NewSlaveNode()
-        sl.MacAddress = fmt.Sprintf("%d", i)
+        sl.MacAddress = fmt.Sprintf("%d%d:%d%d:%d%d:%d%d:%d%d:%d%d", i, i, i, i, i, i, i, i, i, i, i, i)
+        sl.SlaveUUID = ui
+        uuidList = append(uuidList, ui)
         c.Assert(InsertSlaveNode(sl), IsNil)
     }
-
-    var nodes = []SlaveNode{}
-
     for i := 0; i < allNodeCount; i++ {
-        ui := fmt.Sprintf("%d", i)
-        SharedRecordGate().Session().Where(string(SNMFieldUUID + " = ?"), ui).Find(&nodes)
-        //c.Assert(err, IsNil)
+        nodes, err := FindSlaveNode(string(SNMFieldUUID + " = ?"), uuidList[i])
+        c.Assert(err, IsNil)
         c.Assert(len(nodes), Equals, 1)
-        c.Assert(nodes[0].SlaveUUID, Equals, ui)
+        c.Assert(nodes[0].MacAddress, Equals, fmt.Sprintf("%d%d:%d%d:%d%d:%d%d:%d%d:%d%d", i, i, i, i, i, i, i, i, i, i, i, i))
     }
 }
-*/
 
 func (s *RecordSuite) TestNodeNameCandiate(c *C) {
     for i := 0; i < allNodeCount; i++ {
