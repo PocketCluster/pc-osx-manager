da03e43c9c063f9254964c4c40f57c4a4d401df9
stkim1 <stkim1@colorfulglue.com>
2017-05-23 11:17:46 +0900

    test added

--- PC-APP-V2/PC-CORE/beacon/discarded_test.go
+++ PC-APP-V2/PC-CORE/beacon/discarded_test.go
@@ -1,1 +1,46 @@
 package beacon
+
+import (
+    "testing"
+    "time"
+
+    "github.com/stkim1/pc-core/model"
+)
+
+func Test_Discard_Shutdown(t *testing.T) {
+    setUp()
+    defer tearDown()
+
+    // --- VARIABLE PREP ---
+    var (
+        debugComm = &DebugCommChannel{}
+        masterTS  = time.Now()
+        mb, err   = NewMasterBeacon(MasterInit, model.NewSlaveNode(slaveSanitizer), debugComm)
+    )
+    if err != nil {
+        t.Errorf(err.Error())
+        return
+    }
+    if mb.CurrentState() != MasterInit {
+        t.Error("[ERR] Master state is expected to be " + MasterInit.String() + ". Current : " + mb.CurrentState().String())
+        return
+    }
+
+    // --- TX ACTION FAIL ---
+    for i := 0; i <= int(TxActionLimit); i++ {
+        masterTS = masterTS.Add(time.Millisecond + UnboundedTimeout)
+        err = mb.TransitionWithTimestamp(masterTS)
+        if err != nil {
+            t.Log(err.Error())
+        }
+    }
+    if mb.CurrentState() != MasterDiscarded {
+        t.Error("[ERR] Master state is expected to be " + MasterDiscarded.String() + ". Current : " + mb.CurrentState().String())
+        return
+    }
+    // check if mb shutdown works ok
+    mb.Shutdown()
+    if mb.SlaveNode() != nil {
+        t.Error("[ERR] slavenode should be null after discard")
+    }
+}
--- PC-APP-V2/PC-CORE/beacon/manager_test.go
+++ PC-APP-V2/PC-CORE/beacon/manager_test.go
@@ -13,6 +13,7 @@ import (
     "github.com/stkim1/pc-node-agent/slagent"
     "github.com/stkim1/udpnet/mcast"
     "github.com/stkim1/udpnet/ucast"
+    "github.com/stkim1/pc-node-agent/slcontext"
 )
 
 const (
@@ -43,13 +44,14 @@ func (s *ManagerSuite) TearDownTest(c *C) {
 
 // --- test ---
 
-func insertTestNodes(c *C) []string {
+func insertTestNodes(nodeCount int, c *C) []string {
     var uuidList []string = []string{}
-    for i := 0; i < allNodeCount; i++ {
+    for i := 0; i < nodeCount; i++ {
         sl := model.NewSlaveNode(nil)
         sl.NodeName = fmt.Sprintf("pc-node%d", (i * 2) + 1)
         sl.MacAddress = fmt.Sprintf("%d%d:%d%d:%d%d:%d%d:%d%d:%d%d", i, i, i, i, i, i, i, i, i, i, i, i)
         sl.PublicKey = pcrypto.TestSlaveNodePublicKey()
+        sl.Arch = runtime.GOARCH
         err := sl.JoinSlave()
         c.Assert(err, IsNil)
         uuidList = append(uuidList, sl.SlaveUUID)
@@ -61,7 +63,7 @@ func (s *ManagerSuite) TestLoadingNodes(c *C) {
     var (
         nodeFound = false
         comm = &DebugCommChannel{}
-        uuidList = insertTestNodes(c)
+        uuidList = insertTestNodes(allNodeCount, c)
         man, err = NewBeaconManager(masterAgentName, comm)
     )
     c.Assert(err, IsNil)
@@ -77,6 +79,41 @@ func (s *ManagerSuite) TestLoadingNodes(c *C) {
         }
         c.Assert(nodeFound, Equals, true)
     }
+}
+
+func (s *ManagerSuite) TestNameGeneration(c *C) {
+    var (
+        _ = insertTestNodes(allNodeCount, c)
+        comm = &DebugCommChannel{}
+        sa *model.SlaveNode
+        man, err = NewBeaconManager(masterAgentName, comm)
+    )
+    c.Assert(err, IsNil)
+    c.Assert(len(man.(*beaconManger).beaconList), Equals, allNodeCount)
+
+    for i := 0; i < allNodeCount; i++ {
+        sa = model.NewSlaveNode(man.(*beaconManger))
+        mb, err := NewMasterBeacon(MasterInit, sa, comm)
+        c.Assert(err, IsNil)
+        man.(*beaconManger).beaconList = append(man.(*beaconManger).beaconList, mb)
+        // assign new name
+        assignSlaveNodeName(man.(*beaconManger), sa)
+        // check names are in order of 2,4,6,8
+        c.Assert(sa.NodeName, Equals, fmt.Sprintf("pc-node%d", (i + 1) * 2))
+    }
+}
+
+func (s *ManagerSuite) TestShutdown(c *C) {
+    var (
+        _ = insertTestNodes(allNodeCount, c)
+        comm = &DebugCommChannel{}
+        man, err = NewBeaconManager(masterAgentName, comm)
+    )
+    c.Assert(err, IsNil)
+    c.Assert(len(man.(*beaconManger).beaconList), Equals, allNodeCount)
+
+    man.Shutdown()
+    c.Assert(len(man.(*beaconManger).beaconList), Equals, 0)
 }
 
 func (s *ManagerSuite) TestBindBrokenAndTooManyTrialDiscard(c *C) {
@@ -181,37 +218,67 @@ func (s *ManagerSuite) TestBindInitAndTooManyTrialDiscard(c *C) {
     c.Assert(len(man.(*beaconManger).beaconList), Equals, 0)
 }
 
-func (s *ManagerSuite) TestNameGeneration(c *C) {
+func (s *ManagerSuite) TestBindBroken_To_Bounded(c *C) {
     var (
-        _ = insertTestNodes(c)
+        _ = insertTestNodes(1, c)
         comm = &DebugCommChannel{}
-        sa *model.SlaveNode
+        masterTS, slaveTS time.Time = time.Now(), time.Now()
         man, err = NewBeaconManager(masterAgentName, comm)
     )
     c.Assert(err, IsNil)
-    c.Assert(len(man.(*beaconManger).beaconList), Equals, allNodeCount)
+    c.Assert(len(man.(*beaconManger).beaconList), Equals, 1)
+    c.Assert(man.(*beaconManger).beaconList[0].SlaveNode(), NotNil)
+    c.Assert(man.(*beaconManger).beaconList[0].CurrentState(), Equals, MasterBindBroken)
 
-    for i := 0; i < allNodeCount; i++ {
-        sa = model.NewSlaveNode(man.(*beaconManger))
-        mb, err := NewMasterBeacon(MasterInit, sa, comm)
-        c.Assert(err, IsNil)
-        man.(*beaconManger).beaconList = append(man.(*beaconManger).beaconList, mb)
-        // assign new name
-        assignSlaveNodeName(man.(*beaconManger), sa)
-        // check names are in order of 2,4,6,8
-        c.Assert(sa.NodeName, Equals, fmt.Sprintf("pc-node%d", (i + 1) * 2))
+    piface, err := slcontext.PrimaryNetworkInterface()
+    c.Assert(err, IsNil)
+
+    meta := &slagent.PocketSlaveAgentMeta{
+        MetaVersion:         slagent.SLAVE_META_VERSION,
+        MasterBoundAgent:    masterAgentName,
+        SlaveID:             "00:00:00:00:00:00",
+        DiscoveryAgent:      &slagent.PocketSlaveDiscovery {
+            Version:             slagent.SLAVE_DISCOVER_VERSION,
+            SlaveResponse:       slagent.SLAVE_LOOKUP_AGENT,
+            SlaveAddress:        piface.PrimaryIP4Addr(),
+            SlaveGateway:        piface.GatewayAddr,
+        },
     }
-}
-
-func (s *ManagerSuite) TestShutdown(c *C) {
-    var (
-        _ = insertTestNodes(c)
-        comm = &DebugCommChannel{}
-        man, err = NewBeaconManager(masterAgentName, comm)
-    )
+    psm, err := slagent.PackedSlaveMeta(meta)
     c.Assert(err, IsNil)
-    c.Assert(len(man.(*beaconManger).beaconList), Equals, allNodeCount)
 
-    man.Shutdown()
-    c.Assert(len(man.(*beaconManger).beaconList), Equals, 0)
+    // check if this successfully generate new beacon and move the transition
+    err = man.TransitionWithSearchData(mcast.CastPack{Address:*slaveAddr, Message:psm}, masterTS)
+    c.Assert(err, IsNil)
+    c.Assert(len(man.(*beaconManger).beaconList), Equals, 1)
+    c.Assert(man.(*beaconManger).beaconList[0].SlaveNode(), NotNil)
+    c.Assert(man.(*beaconManger).beaconList[0].CurrentState(), Equals, MasterBindRecovery)
+
+    // prepare slave node data
+    slaveTS = masterTS.Add(time.Second)
+    mb := man.(*beaconManger).beaconList[0]
+    aescryptor := mb.(*masterBeacon).state.(DebugState).AESCryptor()
+
+    // create slave meta
+    sa, err := slagent.SlaveBoundedStatus("pc-node1", mb.SlaveNode().SlaveUUID, slaveTS)
+    c.Assert(err, IsNil)
+    mp, err := slagent.PackedSlaveStatus(sa)
+    c.Assert(err, IsNil)
+    encrypted, err := aescryptor.EncryptByAES(mp)
+    c.Assert(err, IsNil)
+    ma := &slagent.PocketSlaveAgentMeta{
+        MetaVersion:         slagent.SLAVE_META_VERSION,
+        MasterBoundAgent:    masterAgentName,
+        SlaveID:             "00:00:00:00:00:00",
+        EncryptedStatus:     encrypted,
+    }
+    psa, err := slagent.PackedSlaveMeta(ma)
+    c.Assert(err, IsNil)
+
+    // test slave meta
+    masterTS = slaveTS.Add(time.Second)
+    err = man.TransitionWithBeaconData(ucast.BeaconPack{Address:*slaveAddr, Message:psa}, masterTS)
+    c.Assert(err, IsNil)
+    c.Assert(len(man.(*beaconManger).beaconList), Equals, 1)
+    c.Assert(man.(*beaconManger).beaconList[0].CurrentState(), Equals, MasterBounded)
 }
