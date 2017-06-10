4cdcae6aa79da7897f825d51bf436534c3989c98
stkim1 <stkim1@colorfulglue.com>
2017-06-10 10:09:38 +0900

    supervisor refactoring

--- PC-APP-V2/pc-node-agent/service/supervisor.go
+++ PC-APP-V2/pc-node-agent/service/supervisor.go
@@ -12,11 +12,94 @@ const (
     stateStarted
 )
 
+// Event is a special service event that can be generated
+// by various goroutines in the application
+type Event struct {
+    Name    string
+    Payload interface{}
+}
+
+type waiter struct {
+    eventC  chan Event
+    cancelC chan struct{}
+}
+
+type Service interface {
+    // These indicate that the service is named and should be individually managed to start.
+    // Plus, only named service can be rebuild
+    Name() string
+    Rebuild() error
+    IsNamedCycle() bool
+
+    Serve() error
+    OnExit(callback func(interface{})) error
+}
+
+// ServerOption is a functional option passed to the server
+type ServiceOption func(s Service) error
+
+type ServeFunc func() error
+func (s ServeFunc) Serve() error {
+    return s()
+}
+
+type OnExitFunc func(callback func(interface{})) error
+func (o OnExitFunc) OnExit(callback func(interface{})) error {
+    return o(callback)
+}
+
+type AppSupervisor interface {
+    Register(srv Service)
+    RegisterFunc(fn ServeFunc)
+    BroadcastEvent(event Event)
+    WaitForEvent(name string, eventC chan Event, cancelC chan struct{})
+    ServiceCount() int
+    IsStopped() bool
+    StopChannel() <- chan struct{}
+    Start() error
+    Stop() error
+    Wait() error
+    OnExit(callback func(interface{}))
+
+    RegisterServiceWithFuncs(sfn ServeFunc, efn OnExitFunc, options...ServiceOption) error
+}
+
+// --- private unexpose methods --- //
+
+type srvcFuncs struct {
+    name       string
+    ServeFunc
+    OnExitFunc
+}
+
+func (s *srvcFuncs) Name() string {
+    return s.name
+}
+
+func (s *srvcFuncs) IsNamedCycle() bool {
+    return (len(s.name) != 0)
+}
+
+func (s *ServeFunc) Rebuild() error {
+    return nil
+}
+
+func MakeServiceNamed(name string) ServiceOption {
+    return func(s Service) error {
+        srv, ok := s.(*srvcFuncs)
+        if ok {
+            srv.name = name
+            return nil
+        }
+        return errors.Errorf("[ERR] invalid type to make cycle async")
+    }
+}
+
 // NewSupervisor returns new instance of initialized supervisor
 func NewAppSupervisor() AppSupervisor {
     srv := &appSupervisor{
         state:           stateStopped,
-        services:        []*Service{},
+        services:        []Service{},
         serviceWG:       &sync.WaitGroup{},
 
         events:          map[string]Event{},
@@ -32,48 +115,12 @@ func NewAppSupervisor() AppSupervisor {
     return srv
 }
 
-// Event is a special service event that can be generated
-// by various goroutines in the application
-type Event struct {
-    Name    string
-    Payload interface{}
-}
-
-type waiter struct {
-    eventC  chan Event
-    cancelC chan struct{}
-}
-
-type Service interface {
-    Serve() error
-}
-
-type ServiceFunc func() error
-
-func (s ServiceFunc) Serve() error {
-    return s()
-}
-
-type AppSupervisor interface {
-    Register(srv Service)
-    RegisterFunc(fn ServiceFunc)
-    BroadcastEvent(event Event)
-    WaitForEvent(name string, eventC chan Event, cancelC chan struct{})
-    ServiceCount() int
-    IsStopped() bool
-    StopChannel() <- chan struct{}
-    Start() error
-    Stop() error
-    Wait() error
-    OnExit(callback func(interface{}))
-}
-
 type appSupervisor struct {
     state           int
     sync.Mutex
 
     serviceWG       *sync.WaitGroup
-    services        []*Service
+    services        []Service
 
     events          map[string]Event
     eventsC         chan Event
@@ -117,9 +164,9 @@ func (p *appSupervisor) fanOut() {
     }
 }
 
-func (p *appSupervisor) serve(service *Service) {
+func (p *appSupervisor) serve(service Service) {
     // this func will be called _after_ a service stops running:
-    removeService := func(srv *Service) {
+    removeService := func(srv Service) {
         p.Lock()
         defer p.Unlock()
         for i, el := range p.services {
@@ -128,36 +175,60 @@ func (p *appSupervisor) serve(service *Service) {
                 break
             }
         }
-        log.Debugf("[APPLICATION] Service %v is done (%v)", *srv, len(p.services))
+        log.Debugf("[SUPERVISOR] service '%s' is removed", srv.Name())
+        log.Debugf("             total %d remained", len(p.services))
     }
 
     p.serviceWG.Add(1)
-    go func(srv *Service) {
-        defer p.serviceWG.Done()
-        defer removeService(srv)
+    go func(srv Service, delSrv func(srv Service), wg *sync.WaitGroup) {
+        defer wg.Done()
 
-        log.Debugf("[APPLICATION] Service %v started (%v)", *srv, p.ServiceCount())
-        err := (*srv).Serve()
+        log.Debugf("[SUPERVISOR] Service %v started (%v)", srv, p.ServiceCount())
+        err := srv.Serve()
         if err != nil {
             log.Debug(errors.WithStack(err))
         }
-    }(service)
+        err = srv.OnExit(nil)
+        if err != nil {
+            log.Debug(errors.WithStack(err))
+        }
+        if !srv.IsNamedCycle() {
+            delSrv(srv)
+        }
+        log.Debugf("[SUPERVISOR] service %s (%v) exits\n", srv.Name(), srv)
+    }(service, removeService, p.serviceWG)
 }
 
 func (p *appSupervisor) Register(srv Service) {
     p.Lock()
     defer p.Unlock()
-    p.services = append(p.services, &srv)
+    p.services = append(p.services, srv)
 
-    log.Debugf("[APPLICATION] Service %v added (%v)", srv, len(p.services))
+    log.Debugf("[SUPERVISOR] Service %v added (%v)", srv, len(p.services))
 
-    if p.state == stateStarted {
-        p.serve(&srv)
+    if p.state == stateStarted && !srv.IsNamedCycle() {
+        p.serve(srv)
     }
 }
 
-func (p *appSupervisor) RegisterFunc(fn ServiceFunc) {
-    p.Register(fn)
+// FIXME : this is to be deprecated
+func (p *appSupervisor) RegisterFunc(fn ServeFunc) {
+}
+
+func (p *appSupervisor) RegisterServiceWithFuncs(sfn ServeFunc, efn OnExitFunc, options...ServiceOption) error {
+    srv := &srvcFuncs {
+        ServeFunc:     sfn,
+        OnExitFunc:    efn,
+    }
+
+    for _, opt := range options {
+        if err := opt(srv); err != nil {
+            return errors.WithStack(err)
+        }
+    }
+
+    p.Register(srv)
+    return nil
 }
 
 func (p *appSupervisor) BroadcastEvent(event Event) {
@@ -165,7 +236,6 @@ func (p *appSupervisor) BroadcastEvent(event Event) {
     defer p.Unlock()
 
     p.events[event.Name] = event
-//    log.Debugf("[APPLICATION] BroadcastEvent: %v", &event)
 
     go func() {
         p.eventsC <- event
@@ -209,6 +279,10 @@ func (p *appSupervisor) StopChannel() <- chan struct{} {
 
 func (p *appSupervisor) Start() error {
     p.Lock()
+    if p.state == stateStarted {
+        defer p.Unlock()
+        return nil
+    }
     defer p.Unlock()
     p.state = stateStarted
 
@@ -231,8 +305,8 @@ func (s *appSupervisor) Stop() error {
         defer s.Unlock()
         return nil
     }
+    defer s.Unlock()
     s.state = stateStopped
-    s.Unlock()
 
     log.Warning("[SUPERVISOR] stopping services...")
     // we broadcast stopping and wait for all goroutines closed with event channels intact to give grace period
---
+++ PC-APP-V2/pc-node-agent/service/supervisor_test.go
@@ -0,0 +1,195 @@
+package service
+
+import (
+    "testing"
+
+    . "gopkg.in/check.v1"
+    log "github.com/Sirupsen/logrus"
+)
+
+const (
+    exitValue       = "no_error"
+    testService1    = "test_service1"
+    testService2    = "test_service2"
+    testService3    = "test_service3"
+)
+
+func TestSupervisor(t *testing.T) { TestingT(t) }
+
+type SupervisorSuite struct {
+    app    AppSupervisor
+}
+
+var _ = Suite(&SupervisorSuite{})
+
+func (s *SupervisorSuite) SetUpSuite(c *C) {
+    log.SetLevel(log.DebugLevel)
+}
+
+func (s *SupervisorSuite) TearDownSuite(c *C) {
+}
+
+func (s *SupervisorSuite) SetUpTest(c *C) {
+    s.app = NewAppSupervisor()
+}
+
+func (s *SupervisorSuite) TearDownTest(c *C) {
+    err := s.app.Stop()
+    c.Assert(err, IsNil)
+    s.app = nil
+}
+
+/// ---
+
+func (s *SupervisorSuite) TestStartStop(c *C) {
+    err := s.app.Start()
+    c.Assert(err, IsNil)
+
+    err = s.app.Stop()
+    c.Assert(err, IsNil)
+}
+
+func (s *SupervisorSuite) TestReigster_Irrepeatable_Service_AfterServiceStart(c *C) {
+    var(
+        exitLatch = make(chan bool)
+        exitChecker = ""
+    )
+    err := s.app.Start()
+    c.Assert(err, IsNil)
+
+    err = s.app.RegisterServiceWithFuncs(
+        func() error {
+            for {
+                select {
+                    case <- s.app.StopChannel():
+                        return nil
+                    default:
+                }
+            }
+        },
+        func(_ func(interface{})) error {
+            exitChecker = exitValue
+            exitLatch <- true
+            return nil
+        })
+    c.Assert(err, IsNil)
+
+    err = s.app.Stop()
+    c.Assert(err, IsNil)
+    <-exitLatch
+
+    c.Check(exitChecker, Equals, exitValue)
+    c.Assert(s.app.ServiceCount(), Equals, 0)
+    close(exitLatch)
+}
+
+func (s *SupervisorSuite) Test_UnnamedService(c *C) {
+    var(
+        exitLatch = make(chan bool)
+        exitChecker = ""
+    )
+    err := s.app.RegisterServiceWithFuncs(
+        func() error {
+            for {
+                select {
+                    case <- s.app.StopChannel():
+                        return nil
+                    default:
+                }
+            }
+        },
+        func(_ func(interface{})) error {
+            exitChecker = exitValue
+            exitLatch <- true
+            return nil
+        })
+    c.Assert(err, IsNil)
+    c.Assert(s.app.ServiceCount(), Equals, 1)
+
+    err = s.app.Start()
+    c.Assert(err, IsNil)
+
+    err = s.app.Stop()
+    c.Assert(err, IsNil)
+    <-exitLatch
+
+    c.Check(exitChecker, Equals, exitValue)
+    c.Assert(s.app.ServiceCount(), Equals, 0)
+    close(exitLatch)
+}
+
+func (s *SupervisorSuite) Test_NamedService_Unsycned_Stop(c *C) {
+    var(
+        exitSignal = make(chan bool)
+        exitLatch = make(chan bool)
+        exitChecker = ""
+    )
+    err := s.app.RegisterServiceWithFuncs(
+        func() error {
+            for {
+                select {
+                    case <- exitSignal:
+                        log.Debug("finishing serve()...")
+                        return nil
+                    default:
+                }
+            }
+        },
+        func(_ func(interface{})) error {
+            exitChecker = exitValue
+            exitLatch <- true
+            return nil
+        },
+        MakeServiceNamed(testService1),
+    )
+    c.Assert(err, IsNil)
+    c.Assert(s.app.ServiceCount(), Equals, 1)
+
+    err = s.app.Start()
+    c.Assert(err, IsNil)
+
+    exitSignal <- true
+    <-exitLatch
+
+    c.Check(exitChecker, Equals, exitValue)
+    c.Assert(s.app.ServiceCount(), Equals, 1)
+    close(exitLatch)
+}
+
+func (s *SupervisorSuite) Test_NamedService_Sycned_Stop(c *C) {
+    var(
+        exitLatch = make(chan bool)
+        exitChecker = ""
+    )
+    err := s.app.RegisterServiceWithFuncs(
+        func() error {
+            for {
+                select {
+                    case <- s.app.StopChannel():
+                        log.Debug("finishing serve()...")
+                        return nil
+                    default:
+                }
+            }
+        },
+        func(_ func(interface{})) error {
+            exitChecker = exitValue
+            exitLatch <- true
+            return nil
+        },
+        MakeServiceNamed(testService2),
+    )
+    c.Assert(err, IsNil)
+    c.Assert(s.app.ServiceCount(), Equals, 1)
+
+    err = s.app.Start()
+    c.Assert(err, IsNil)
+
+    err = s.app.Stop()
+    c.Assert(err, IsNil)
+    <-exitLatch
+
+    c.Check(exitChecker, Equals, exitValue)
+    c.Assert(s.app.ServiceCount(), Equals, 1)
+    close(exitLatch)
+}
