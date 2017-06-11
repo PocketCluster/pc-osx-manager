19385c64a0c72fa532e857b5aa8d8426c2b768d1
stkim1 <stkim1@colorfulglue.com>
2017-06-12 03:51:30 +0900

    test fix

--- PC-APP-V2/pc-node-agent/service/supervisor_event_test.go
+++ PC-APP-V2/pc-node-agent/service/supervisor_event_test.go
@@ -20,7 +20,7 @@ const (
     testValue4 = "test_value4"
 )
 
-func (s *SupervisorSuite) Test_UnamedService_Receive_Event(c *C) {
+func (s *SupervisorSuite) Test_UnamedService_Receive_MultiEvent(c *C) {
     var(
         exitLatch = make(chan string)
         eventLatch = make(chan string)
@@ -82,7 +82,114 @@ func (s *SupervisorSuite) Test_UnamedService_Receive_Event(c *C) {
     close(exitLatch)
     close(eventLatch)
 
+    // check if water queue is empty
+    c.Assert(len(s.app.(*appSupervisor).eventWaiters), Equals, 3)
     c.Assert(len(s.app.(*appSupervisor).eventWaiters[testEvent1]), Equals, 0)
     c.Assert(len(s.app.(*appSupervisor).eventWaiters[testEvent2]), Equals, 0)
     c.Assert(len(s.app.(*appSupervisor).eventWaiters[testEvent3]), Equals, 0)
+}
+
+func (s *SupervisorSuite) Test_Multiple_UnamedService_Receive_Event(c *C) {
+    var(
+        exitLatch = make(chan string)
+        eventLatch = make(chan string)
+
+        eventC1 = make(chan Event)
+        eventC2 = make(chan Event)
+        eventC3 = make(chan Event)
+    )
+    err := s.app.Start()
+    c.Assert(err, IsNil)
+
+    err = s.app.RegisterServiceWithFuncs(
+        func() error {
+            for {
+                select {
+                    case e := <-eventC1: {
+                        eventLatch <- e.Payload.(string)
+                    }
+                    case <- s.app.StopChannel(): {
+                        return nil
+                    }
+                    default:
+                }
+            }
+        },
+        func(_ func(interface{})) error {
+            exitLatch <- exitValue
+            return nil
+        },
+        BindEventWithService(testEvent1, eventC1),
+    )
+    c.Assert(err, IsNil)
+    c.Assert(s.app.ServiceCount(), Equals, 1)
+
+
+    err = s.app.RegisterServiceWithFuncs(
+        func() error {
+            for {
+                select {
+                    case e := <-eventC2: {
+                        eventLatch <- e.Payload.(string)
+                    }
+                    case <- s.app.StopChannel(): {
+                        return nil
+                    }
+                    default:
+                }
+            }
+        },
+        func(_ func(interface{})) error {
+            exitLatch <- exitValue
+            return nil
+        },
+        BindEventWithService(testEvent1, eventC2),
+    )
+    c.Assert(err, IsNil)
+    c.Assert(s.app.ServiceCount(), Equals, 2)
+
+    err = s.app.RegisterServiceWithFuncs(
+        func() error {
+            for {
+                select {
+                    case e := <-eventC3: {
+                        eventLatch <- e.Payload.(string)
+                    }
+                    case <- s.app.StopChannel(): {
+                        return nil
+                    }
+                    default:
+                }
+            }
+        },
+        func(_ func(interface{})) error {
+            exitLatch <- exitValue
+            return nil
+        },
+        BindEventWithService(testEvent1, eventC3),
+    )
+    c.Assert(err, IsNil)
+    c.Assert(s.app.ServiceCount(), Equals, 3)
+
+
+    s.app.BroadcastEvent(Event{Name:testEvent1, Payload:testValue1})
+    c.Assert(<-eventLatch, Equals, testValue1)
+    c.Assert(<-eventLatch, Equals, testValue1)
+    c.Assert(<-eventLatch, Equals, testValue1)
+
+    err = s.app.Stop()
+    c.Assert(err, IsNil)
+    c.Check(<-exitLatch, Equals, exitValue)
+    c.Check(<-exitLatch, Equals, exitValue)
+    c.Check(<-exitLatch, Equals, exitValue)
+
+    // it takes abit to
+    time.Sleep(time.Second)
+    c.Assert(s.app.ServiceCount(), Equals, 0)
+    close(exitLatch)
+    close(eventLatch)
+
+    // check if water queue is empty
+    c.Assert(len(s.app.(*appSupervisor).eventWaiters), Equals, 1)
+    c.Assert(len(s.app.(*appSupervisor).eventWaiters[testEvent1]), Equals, 0)
 }
