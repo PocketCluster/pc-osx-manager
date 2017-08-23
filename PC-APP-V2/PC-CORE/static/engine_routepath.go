2da2dc05d169e90637c5dee710296ceb33524444
stkim1 <stkim1@colorfulglue.com>
2017-08-24 01:49:22 +0900

    package list api client

--- PC-APP-V2/PC-CORE/static/engine_routepath.go
+++ PC-APP-V2/PC-CORE/static/engine_routepath.go
@@ -167,13 +167,31 @@ func initRoutePathService() {
 
     // get the list of available packages
     theApp.GET(routepath.RpathPackageList(), func(_, path, _ string) error {
+        var (
+            feedError = func(errMsg string) error {
+                data, err := json.Marshal(ReponseMessage{
+                    "package-list": {
+                        "status": false,
+                        "error" : errMsg,
+                    },
+                })
+                if err != nil {
+                    log.Debugf(err.Error())
+                    return errors.WithStack(err)
+                }
+                err = FeedResponseForGet(path, string(data))
+                return errors.WithStack(err)
+            }
+
+            pkgList = []map[string]interface{}{}
+        )
 
         req, err :=  http.NewRequest("GET", "https://api.pocketcluster.io/service/v014/package/list", nil)
         if err != nil {
             log.Debugf(errors.WithStack(err).Error())
-            return errors.WithStack(err)
+            return feedError("Unable to access package list. Reason : " + errors.WithStack(err).Error())
         }
-        req.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
+        //req.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
         req.Header.Add("User-Agent", "PocketCluster/0.1.4 (OSX)")
         req.Header.Set("Content-Type", " application/json; charset=utf-8")
         client := &http.Client{
@@ -182,26 +200,37 @@ func initRoutePathService() {
         resp, err := client.Do(req)
         if err != nil {
             log.Debugf(errors.WithStack(err).Error())
-            return errors.WithStack(err)
+            return feedError("Unable to access package list. Reason : " + errors.WithStack(err).Error())
         }
         defer resp.Body.Close()
 
+        if resp.StatusCode != 200 {
+            return feedError(errors.Errorf("Service unavailable. Status : %d", resp.StatusCode).Error())
+        }
         var pkgs = []*model.Package{}
         err = json.NewDecoder(resp.Body).Decode(&pkgs)
         if err != nil {
             log.Debugf(errors.WithStack(err).Error())
-            return errors.WithStack(err)
+            return feedError("Unable to access package list. Reason : " + errors.WithStack(err).Error())
         }
-        if len(pkgs) != 0 {
-            err = model.UpdatePackages(pkgs)
-            if err != nil {
-                log.Debugf(errors.WithStack(err).Error())
-            }
+        if len(pkgs) == 0 {
+            return feedError("No package avaiable. Contact us at Slack channel.")
+        } else {
+            // update package doesn't return error when there is packages to update.
+            model.UpdatePackages(pkgs)
         }
 
+        for i, _ := range pkgs {
+            pkgList = append(pkgList, map[string]interface{} {
+                "description": pkgs[i].Description,
+                "id" : pkgs[i].PkgID,
+                "installed": false,
+            })
+        }
         data, err := json.Marshal(ReponseMessage{
             "package-list": {
                 "status": true,
+                "list":   pkgList,
             },
         })
         if err != nil {
