abbb02e621c645a5e426e1e62b167171eecdec9f
stkim1 <stkim1@colorfulglue.com>
2017-09-12 16:24:38 +0900

    sync command added

--- PC-APP-V2/PC-CORE/static/engine_install.go
+++ PC-APP-V2/PC-CORE/static/engine_install.go
@@ -1,18 +1,71 @@
 package main
 
 import (
+    "bytes"
+    "encoding/binary"
     "encoding/json"
     "fmt"
+    "io"
     "io/ioutil"
     "net/http"
     "time"
 
     log "github.com/Sirupsen/logrus"
     "github.com/pkg/errors"
+    "github.com/Redundancy/go-sync"
     //"github.com/stkim1/pc-core/context"
     "github.com/stkim1/pc-core/event/route/routepath"
     "github.com/stkim1/pc-core/model"
 )
+
+// reads the file headers and checks the magic string, then the semantic versioning
+// return : in order of 'filesize', 'blocksize', 'blockcount', 'rootHash', 'error'
+func readHeadersAndCheck(r io.Reader) (int64, uint32, uint32, []byte, error) {
+    var (
+        bMagic                []byte = make([]byte, len(gosync.PocketSyncMagicString))
+        major, minor, patch   uint16 = 0, 0, 0
+        filesize              int64  = 0
+        blocksize, blockcount uint32 = 0, 0
+        hLen                  uint32 = 0
+        rootHash              []byte = nil
+    )
+    // magic string
+    if _, err := r.Read(bMagic); err != nil {
+        return 0, 0, 0, nil, errors.WithStack(err)
+    } else if string(bMagic) != gosync.PocketSyncMagicString {
+        return 0, 0, 0, nil, errors.New("meta header does not confirm. Not a valid meta")
+    }
+
+    // version
+    for _, v := range []*uint16{&major, &minor, &patch} {
+        if err := binary.Read(r, binary.LittleEndian, v); err != nil {
+            return 0, 0, 0, nil, errors.WithStack(err)
+        }
+    }
+    if major != gosync.PocketSyncMajorVersion || minor != gosync.PocketSyncMinorVersion || patch != gosync.PocketSyncPatchVersion {
+        return 0, 0, 0, nil, errors.Errorf("The acquired version (%v.%v.%v) does not match the tool (%v.%v.%v).",
+            major, minor, patch,
+            gosync.PocketSyncMajorVersion, gosync.PocketSyncMinorVersion, gosync.PocketSyncPatchVersion)
+    }
+
+    if err := binary.Read(r, binary.LittleEndian, &filesize); err != nil {
+        return 0, 0, 0, nil, errors.WithStack(err)
+    }
+    if err := binary.Read(r, binary.LittleEndian, &blocksize); err != nil {
+        return 0, 0, 0, nil, errors.WithStack(err)
+    }
+    if err := binary.Read(r, binary.LittleEndian, &blockcount); err != nil {
+        return 0, 0, 0, nil, errors.WithStack(err)
+    }
+    if err := binary.Read(r, binary.LittleEndian, &hLen); err != nil {
+        return 0, 0, 0, nil, errors.WithStack(err)
+    }
+    rootHash = make([]byte, hLen)
+    if _, err := r.Read(rootHash); err != nil {
+        return 0, 0, 0, nil, errors.WithStack(err)
+    }
+    return filesize, blocksize, blockcount, rootHash, nil
+}
 
 func initInstallRoutePath() {
     const (
@@ -129,10 +182,12 @@ func initInstallRoutePath() {
         return nil
     })
 
+
     // install a package
     theApp.POST(routepath.RpathPackageInstall(), func(_, rpath, payload string) error {
         var (
             feedError = func(errMsg string) error {
+                log.Errorf(errMsg)
                 data, err := json.Marshal(ReponseMessage{
                     "package-install": {
                         "status": false,
@@ -170,32 +225,67 @@ func initInstallRoutePath() {
         // --- --- --- --- --- download meta first --- --- --- --- ---
         metaReq, err := newRequest(fmt.Sprintf("https://api.pocketcluster.io%s", pkg.MetaURL), false)
         if err != nil {
-            log.Debugf(errors.WithStack(err).Error())
             return feedError("Unable to access package meta data. Reason : " + errors.WithStack(err).Error())
         }
         _, err = readRequest(metaReq, client)
         if err != nil {
-            log.Debugf(errors.WithStack(err).Error())
-            return feedError("Unable to parse package meta data. Reason : " + errors.WithStack(err).Error())
+            return feedError("Unable to access package meta data. Reason : " + errors.WithStack(err).Error())
         }
 
         //  --- --- --- --- --- download repo list --- --- --- --- ---
         repoReq, err := newRequest("https://api.pocketcluster.io/service/v014/package/repo", false)
         if err != nil {
-            log.Debugf(errors.WithStack(err).Error())
             return feedError("Unable to access repository list. Reason : " + errors.WithStack(err).Error())
         }
         repoData, err := readRequest(repoReq, client)
         if err != nil {
-            log.Debugf(errors.WithStack(err).Error())
-            return feedError("Unable to parse repository list. Reason : " + errors.WithStack(err).Error())
+            return feedError("Unable to access repository list. Reason : " + errors.WithStack(err).Error())
         }
         err = json.Unmarshal(repoData, &repoList)
         if err != nil {
-            log.Debugf(err.Error())
-            return feedError("Unable to parse repository list. Reason : " + errors.WithStack(err).Error())
+            return feedError("Unable to access repository list. Reason : " + errors.WithStack(err).Error())
         }
-        log.Debugf("repo %v", repoList)
+        if len(repoList) == 0 {
+            return feedError("Unable to access repository list. Reason : image repository unavailable")
+        }
+
+        //  --- --- --- --- --- download core sync --- --- --- --- ---
+        cSyncReq, err := newRequest(fmt.Sprintf("https://api.pocketcluster.io%s", pkg.CoreImageSync), true)
+        if err != nil {
+            return feedError("Unable to access core image sync data. Reason : " + errors.WithStack(err).Error())
+        }
+        cSyncData, err := readRequest(cSyncReq, client)
+        if err != nil {
+            return feedError("Unable to access core image sync data. Reason : " + errors.WithStack(err).Error())
+        }
+        filesize, blocksize, blockcount, rootHash, err := readHeadersAndCheck(bytes.NewBuffer(cSyncData))
+        if err != nil {
+            return feedError("Unable to access core image sync data. Reason : " + errors.WithStack(err).Error())
+        }
+        log.Debugf("FileSize %v | BlockSize %v | BlockCount %v | RootHash %v ", filesize, blocksize, blockcount, rootHash)
+        var coreImgRepoList []string = []string{}
+        for _, r := range repoList {
+            coreImgRepoList = append(coreImgRepoList, fmt.Sprintf("https://%s%s", r, pkg.CoreImageURL))
+        }
+
+        //  --- --- --- --- --- download node sync --- --- --- --- ---
+        nSyncReq, err := newRequest(fmt.Sprintf("https://api.pocketcluster.io%s", pkg.NodeImageSync), true)
+        if err != nil {
+            return feedError("Unable to access node image sync data. Reason : " + errors.WithStack(err).Error())
+        }
+        nSyncData, err := readRequest(nSyncReq, client)
+        if err != nil {
+            return feedError("Unable to access node image sync data. Reason : " + errors.WithStack(err).Error())
+        }
+        filesize, blocksize, blockcount, rootHash, err = readHeadersAndCheck(bytes.NewBuffer(nSyncData))
+        if err != nil {
+            return feedError("Unable to access node image sync data. Reason : " + errors.WithStack(err).Error())
+        }
+        log.Debugf("FileSize %v | BlockSize %v | BlockCount %v | RootHash %v ", filesize, blocksize, blockcount, rootHash)
+        var nodeImgRepoList []string = []string{}
+        for _, r := range repoList {
+            nodeImgRepoList = append(nodeImgRepoList, fmt.Sprintf("https://%s%s", r, pkg.NodeImageURL))
+        }
 
 
         feedError("This is test error")
