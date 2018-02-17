95cee32fb2da4c5c8907591ff519ba77855e5f77
stkim1 <stkim1@colorfulglue.com>
2018-02-17 20:45:54 +0900

    fdisk removed

--- PC-APP-V2/pc-node-agent/exec/pocketd/main.go
+++ PC-APP-V2/pc-node-agent/exec/pocketd/main.go
@@ -11,7 +11,6 @@ import (
     logrusSyslog "github.com/Sirupsen/logrus/hooks/syslog"
     "github.com/gravitational/teleport/lib/sshutils/scp"
     nodeagent "github.com/stkim1/pc-node-agent"
-    "github.com/stkim1/pc-node-agent/utils/disk"
 )
 
 const (
@@ -21,7 +20,6 @@ const (
     // modes list
     modeDhcpAgent string = "dhcpagent"
     modeScpAgent  string = "scp"
-    modePartition string = "fdisk"
     modeVerCheck  string = "--version"
 )
 
@@ -59,34 +57,6 @@ func main() {
                 err := runDhcpAgentReport()
                 if err != nil {
                     log.Error(err.Error())
-                }
-            }
-            // sfdisk
-            case modePartition: {
-                // get the disk layout
-                layout, err := disk.DumpDiskLayout("/dev/mmcblk0")
-                if err != nil {
-                    log.Debug(err)
-                }
-                // Total Disk Sector Count
-                sectorTotalCount, err := disk.TotalDiskSectorCount("mmcblk0")
-                if err != nil {
-                    log.Debug(err)
-                }
-                // Hardware Disk Sector Size
-                sectorUnitSize, err := disk.DiskSectorSizeInByte("mmcblk0")
-                if err != nil {
-                    log.Debug(err)
-                }
-                // physical memory size
-                phymem, err := disk.TotalPhysicalMemSizeInByte()
-                if err != nil {
-                    log.Debug(err)
-                }
-                newLayout := disk.ReformatDiskLayout(layout, sectorTotalCount, sectorUnitSize, phymem, 4)
-                err = disk.RepartitionDisk("/dev/mmcblk0", newLayout)
-                if err != nil {
-                    log.Debug(err)
                 }
             }
             case modeVerCheck: {
