e2783102f42aa0090bf6ce54f345d9298f9e88af
stkim1 <stkim1@colorfulglue.com>
2018-02-23 20:59:05 +0900

    webkit removal

--- PC-APP-V2/PC-APP/manager/manager/WindowController/About/AboutWindow.m
+++ PC-APP-V2/PC-APP/manager/manager/WindowController/About/AboutWindow.m
@@ -7,15 +7,10 @@
 
 #import "AboutWindow.h"
 
-@interface AboutWindow ()
-@end
-
 @implementation AboutWindow
-@synthesize webView;
 
 - (id)initWithWindow:(NSWindow *)window {
     self = [super initWithWindow:window];
-
     return self;
 }
 
@@ -27,38 +22,16 @@
     [self.window setTitlebarAppearsTransparent:YES];
     self.window.styleMask |= NSFullSizeContentViewWindowMask;
 
-    NSString *str = @"<div style=\"text-align:left;font-family:'Helvetica Neue';font-weight:100;font-size:38px;\">PocketCluster</div><div style=\"text-align:left;font-family:Arial;font-size:13px\">Version {VERSION} - Early Evaluation<br/><br/>Copyright &copy;2015,{YEAR} PocketCluster<br/><a href=\"{URL}\">{URL}</a></div>";
-
+    NSString *str = @"Version {VERSION} - Early Evaluation\n\nCopyright ©2015,{YEAR} PocketCluster";
     NSString *dateString = [NSString stringWithCString:__DATE__ encoding:NSASCIIStringEncoding];
     NSString *yearString = [dateString substringWithRange:NSMakeRange([dateString length] - 4, 4)];
 
     str = [str stringByReplacingOccurrencesOfString:@"{YEAR}" withString:yearString];
     str = [str stringByReplacingOccurrencesOfString:@"{VERSION}" withString:[[[NSBundle mainBundle] infoDictionary] valueForKey:@"CFBundleShortVersionString"]];
-    str = [str stringByReplacingOccurrencesOfString:@"{URL}" withString:@"https://github.com/pocketcluster/pocketcluster"];
-    str = [str stringByReplacingOccurrencesOfString:@"\n" withString:@"<br>"];
-
-    //self.webView.policyDelegate = self;
-    [self.webView setDrawsBackground:NO];
-    [self.webView.mainFrame loadHTMLString:str baseURL:nil];
+    [self.copyright setStringValue:str];
 }
 
-#pragma mark - WebView Delegate
-- (void)webView:(WebView*)webView decidePolicyForNavigationAction:(NSDictionary*)actionInformation request:(NSURLRequest*)request frame:(WebFrame*)frame decisionListener:(id<WebPolicyDecisionListener>)listener {
-    NSString *host = [[request URL] host];
-    if(host) {
-        [[NSWorkspace sharedWorkspace] openURL:[request URL]];
-    } else {
-        [listener use];
-    }
+-(IBAction)homepage:(id)sender {
+    
 }
-
-- (void)use {
-}
-
-- (void)download {
-}
-
-- (void)ignore {
-}
-
 @end
